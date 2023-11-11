3f7bc4f03edf830eb04051ec926b91e3093d30ac
Aokijiop <Aaokijiop@gmail.com>
2023-11-10 22:34:01 -0800

    Added login page

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import CoursePage from "./pages/CoursePage";
 
 export default function App() {
   return (
-    <>
+    <ChakraProvider>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/course" element={<CoursePage />} />
       </Routes>
-    </>
+    </ChakraProvider>
   );
 }
--- app/client/src/pages/LoginPage.jsx
+++ app/client/src/pages/LoginPage.jsx
@@ -1,4 +1,71 @@
-export default function LoginPage(){
-    return<>
-    </>
-}
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Box,
+  Text,
+  Heading,
+  StackDivider,
+  Flex,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+import background from "../assets/background.jpg";
+
+const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+        navigate("/home");
+  };
+
+  return (
+    <Flex
+      bgImage={background}
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      w="100vw"
+      h="100vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Card>
+        <CardHeader>
+          <Heading size="md">Login</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing="4">
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Username
+              </Heading>
+              <Input placeholder="Enter username" mt={1}></Input>
+            </Box>
+            <Box>
+              <Heading size="xs" textTransform="uppercase">
+                Password
+              </Heading>
+              <Input placeholder="Enter password" mt={1}></Input>
+            </Box>
+          </Stack>
+          <Button
+            bgColor="green"
+            colorScheme="white"
+            mt={3}
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+        </CardBody>
+      </Card>
+    </Flex>
+  );
+};
+
+export default LoginPage;
